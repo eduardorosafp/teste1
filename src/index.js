35711d03fdc12fbed1f8e67f54485aa6ae04d601
eduardo flores <93412058+eduardorosafp@users.noreply.github.com>
2023-06-02 11:34:15 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimpleLandingPage from './components'
+
+
+const App = () => <SimpleLandingPage/>
+
+ReactDOM.render(<App />, document.querySelector('#root'));
